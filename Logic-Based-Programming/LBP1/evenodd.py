b4ff38a1203b2814432fda2ab975d1425f9ddd2f
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-03 22:17:13 +0530

    Update evenodd.py

--- Logic-Based-Programming/LBP1/evenodd.py
+++ Logic-Based-Programming/LBP1/evenodd.py
@@ -1,21 +1,30 @@
-// Program to check whether the given number is even or odd
-//Input Format
-//number n
-//Constraints
-//n value must be >=0
-//Output Format
-//even or odd invalid
-//Sample Input 0
-//12
-//Sample Output 0
-//even
-//Sample Input 1
-//13
-//Sample Output 1
-//odd
+Problem:
+  
+Program to check whether the given number is even or odd
+
+Input Format
+number n
+
+Constraints
+n value must be >=0
+
+Output Format
+even or odd invalid
+
+Sample Input 0
+12
+
+Sample Output 0
+even
+
+Sample Input 1
+13
+
+Sample Output 1
+odd
 
 
-//Program:
+Solution Program:
   
   n=int(input())
   print("invalid" if n<0 else("even" if n%2==0 else "odd"))
