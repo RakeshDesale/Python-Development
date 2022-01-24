bab3effe41d125e82edc356f61ea5603b5798a83
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-24 19:08:11 +0530

    Update solution2.py

--- Logic-Based-Programming/LBP-032/solution2.py
+++ Logic-Based-Programming/LBP-032/solution2.py
@@ -1,4 +1,10 @@
-# Using ASCII Values
-
 # Solution:
 
+str=input()
+count=0
+for i in str:
+    if (i>='a' and i<='z') or (i>='A' and i<='Z') or (i>='0' and i<='9'):
+        pass
+    else:
+        count+=1
+print (count)
