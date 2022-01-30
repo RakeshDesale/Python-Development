42dfb9433bb348a67ed2a9da0a6bc37da5b2422b
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-30 14:59:41 +0530

    Update solution2.py

--- Logic-Based-Programming/LBP-048/solution2.py
+++ Logic-Based-Programming/LBP-048/solution2.py
@@ -1,2 +1,15 @@
 # Solution:
 
+def convert(s):
+    return ord(s)-48
+
+L=[i for i in input()]
+if L[0]=='0':
+    min=convert(L[1])
+else:
+    min=convert(L[0])*10+convert(L[1])
+if L[3]=='0':
+    sec=convert(L[4])
+else:
+    sec=convert(L[3])*10+convert(L[4])
+print(min*60+sec)
