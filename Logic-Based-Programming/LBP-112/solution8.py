37bd516e3200cc03691fa46de4d661f15abfc7c2
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-03 18:13:17 +0530

    Create solution8.py

---
+++ Logic-Based-Programming/LBP-112/solution8.py
@@ -0,0 +1,29 @@
+# Using Binary Search and Using Function
+
+# Solution:
+
+def searchElement(L,key):
+    index = -1
+    for i in range(0,len(L)):
+        for j in range(i+1,len(L)):
+            if (L[i] > L[j]):
+                temp = L[i]
+                L[i] = L[j]
+                L[j] = temp
+    
+    low = 0
+    high = len(L)-1
+    while (low <= high):
+        mid = (low+high)//2
+        if (key == L[mid]):
+            index = mid
+            break
+        elif (key > L[mid]):
+            low = mid+1
+        else:
+            high = mid-1
+    return index
+    
+n = int(input())
+L = [int(i) for i in input().split()]
+print(searchElement(L,int(input())))
