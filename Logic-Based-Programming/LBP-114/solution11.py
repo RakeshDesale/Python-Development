fed463b8e21dcf99be8f199d475cfe235842b7c1
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-07 22:11:39 +0530

    Create solution11.py

---
+++ Logic-Based-Programming/LBP-114/solution11.py
@@ -0,0 +1,15 @@
+# Traditional Approach Using Function
+
+# Solution:
+
+def minElement(L):
+    for i in range(len(L)):
+        for j in range(i+1,len(L)):
+            if L[i] > L[j]:
+                temp = L[i]
+                L[i] = L[j]
+                L[j] = temp
+    return L[1-1]
+
+n = int(input())
+print(minElement([int(i) for i in input().split()]))
