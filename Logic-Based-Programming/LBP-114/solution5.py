557bca87d241abd4bd44ee491df39d823f8e67eb
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-07 22:06:41 +0530

    Create solution5.py

---
+++ Logic-Based-Programming/LBP-114/solution5.py
@@ -0,0 +1,14 @@
+# Traditional Approach
+
+# Solution:
+
+n = int(input())
+L = [int(i) for i in input().split()]
+for i in range(n):
+    for j in range(i+1,n):
+        if L[i]>L[j]:
+            temp = L[i]
+            L[i] = L[j]
+            L[j] = temp
+
+print(L[1-1])
