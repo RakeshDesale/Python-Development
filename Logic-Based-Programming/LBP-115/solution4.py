77a641d741b249fa28bc3fb7683c9e964dfb5ea0
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-08 21:52:30 +0530

    Create solution4.py

---
+++ Logic-Based-Programming/LBP-115/solution4.py
@@ -0,0 +1,13 @@
+# Using Core Concept
+
+# Solution:
+
+n = int(input())
+L = [int(i) for i in input().split()]
+for i in range(n):
+    for j in range(i+1,n):
+        if L[i]>L[j]:
+            temp = L[i]
+            L[i] = L[j]
+            L[j] = temp
+print(L[n-1]-L[1-1])
