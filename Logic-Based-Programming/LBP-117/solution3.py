6e4040ac28a27d1aac4616316c5640a13b7b2c45
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-11 21:12:33 +0530

    Create solution3.py

---
+++ Logic-Based-Programming/LBP-117/solution3.py
@@ -0,0 +1,11 @@
+# Using Core Concept
+
+# Solution:
+
+n = int(input())
+L = [int(i) for i in input().split()]
+for i in range(n):
+    for j in range(i+1,n):
+        if L[i]>L[j]:
+            L[i],L[j]=L[j],L[i]
+print(L[2-1])
