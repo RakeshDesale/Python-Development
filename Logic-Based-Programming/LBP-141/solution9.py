e31607bf4d0b606b1a7db968810de53cde4c6bc2
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-30 21:56:23 +0530

    Create solution9.py

---
+++ Logic-Based-Programming/LBP-141/solution9.py
@@ -0,0 +1,21 @@
+# Using Core Logic and Using Function Without Return Value
+
+# Solution:
+
+def rearrangeArr(L):
+    for i in range(n):
+        for j in range(i+1,n):
+            if L[i]>L[j]:
+                temp = L[i]
+                L[i] = L[j]
+                L[j] = temp
+
+    low = 0
+    high = n-1
+    while(low <= high):
+        print(L[low],L[high],end=' ')
+        low += 1
+        high -= 1
+
+n = int(input())
+rearrangeArr([int(i) for i in input().split()])
