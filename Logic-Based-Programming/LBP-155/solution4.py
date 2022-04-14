f7834b4f13c970813728ec2619b1d66fdeb44688
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-14 16:23:01 +0530

    Create solution4.py

---
+++ Logic-Based-Programming/LBP-155/solution4.py
@@ -0,0 +1,12 @@
+# Using Function
+
+# Solution:
+
+n = int(input())
+k = int(input())
+L = [int(i) for i in input().split()]
+for i in range(n):
+    for j in range(i+1,n):
+        if L[i] > L[j]:
+            L[i],L[j] = L[j],L[i]
+print(L[k-1])
