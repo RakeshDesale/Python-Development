c1d6e5a5be5cc96ce9991e352996315e1d35b7d3
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-14 16:23:45 +0530

    Update solution4.py

--- Logic-Based-Programming/LBP-155/solution4.py
+++ Logic-Based-Programming/LBP-155/solution4.py
@@ -2,11 +2,10 @@
 
 # Solution:
 
+def kShortExeTime(L,k):
+    L.sort()
+    return L[k-1]
+
 n = int(input())
 k = int(input())
-L = [int(i) for i in input().split()]
-for i in range(n):
-    for j in range(i+1,n):
-        if L[i] > L[j]:
-            L[i],L[j] = L[j],L[i]
-print(L[k-1])
+print(kShortExeTime([int(i) for i in input().split()],k))
