dddac57470b7fca57964acd08e4bc79332cd4ec5
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-20 13:52:51 +0530

    Create solution1.py

---
+++ Logic-Based-Programming/LBP-161/solution1.py
@@ -0,0 +1,17 @@
+# Solution:
+
+def conSeq(L1,L2,n1,n2):
+    L3 = L1 + L2
+    L3.sort()
+    counter = 0
+    for i in range(0,(n1+n2)-1):
+        if(L3[i]+1==L3[i+1]):
+            counter += 1
+    return (counter==(n1+n2)-1)
+
+
+n1 = int(input())
+L1 = [int(i) for i in input().split()]
+n2 = int(input())
+L2 = [int(i) for i in input().split()]
+print(str(conSeq(L1,L2,n1,n2)).lower())
