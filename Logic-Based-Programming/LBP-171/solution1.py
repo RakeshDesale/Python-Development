4682aaab3c18b4b8769750165a6d8743c1d43926
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-26 10:54:10 +0530

    Create solution1.py

---
+++ Logic-Based-Programming/LBP-171/solution1.py
@@ -0,0 +1,5 @@
+# Solution:
+
+L = ["break","case","continue","default","defer","else","for","func","goto","if","map","range","return","struct","type","var"]
+st = input()
+print("true" if st in L else "false")
