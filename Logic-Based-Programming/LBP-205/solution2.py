30226515f9dc9071107149d1e1723c12278e8104
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-28 16:30:12 +0530

    Create solution2.py

---
+++ Logic-Based-Programming/LBP-205/solution2.py
@@ -0,0 +1,20 @@
+# Using Function
+
+# Solution:
+
+def isPrime(n):
+    factors = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            factors += 1
+    return factors == 2
+
+L = []
+sum = 0
+for i in range(3):
+    L.append([int(i) for i in input().split()])
+for i in range(3):
+    for j in range(3):
+        if(isPrime(L[i][j])):
+            sum = sum + L[i][j]
+print(sum)
