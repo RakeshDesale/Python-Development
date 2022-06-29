d1144018cc2d6a5981df8974e042e65aff127591
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-29 17:57:50 +0530

    Create solution2.py

---
+++ Logic-Based-Programming/LBP-206/solution2.py
@@ -0,0 +1,15 @@
+# Using Function
+
+# Solution:
+
+def matrixRowSum(L):
+    for i in range(3):
+        sum = 0
+        for j in range(3):
+            sum = sum + L[i][j]
+        print(sum)
+
+L = []
+for i in range(3):
+    L.append([int(i) for i in input().split()])
+matrixRowSum(L)
