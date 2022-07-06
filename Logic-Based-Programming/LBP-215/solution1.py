45f2a1ac3c1304034e16e85103ce2f49cc4bac5d
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-06 17:22:48 +0530

    Create solution1.py

---
+++ Logic-Based-Programming/LBP-215/solution1.py
@@ -0,0 +1,11 @@
+# Solution:
+
+L = []
+for i in range(3):
+    L.append([int(i) for i in input().split()])
+max = L[0][0]
+for i in range(3):
+    for j in range(3):
+        if max < L[i][j]:
+            max = L[i][j]
+    print(max)
