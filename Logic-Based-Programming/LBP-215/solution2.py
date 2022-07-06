2559f869c182dd14e4ba7eb253de8dfbe35d9788
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-06 17:23:31 +0530

    Create solution2.py

---
+++ Logic-Based-Programming/LBP-215/solution2.py
@@ -0,0 +1,13 @@
+# Using Core Concept
+
+# Solution:
+
+L = []
+for i in range(3):
+    L.append([int(i) for i in input().split()])
+for i in range(3):
+    for j in range(3):
+        for k in range(j + 1, 3):
+            if L[i][j] > L[i][k]:
+                L[i][j], L[i][k] = L[i][k], L[i][j]
+    print(L[i][2])
