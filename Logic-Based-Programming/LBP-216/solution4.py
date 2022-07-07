4ef77d62523f03dc72bcdef789e9372a871ba619
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-07 18:25:18 +0530

    Create solution4.py

---
+++ Logic-Based-Programming/LBP-216/solution4.py
@@ -0,0 +1,16 @@
+# Using Function and Using Core Concept
+
+# Solution:
+
+def minEle(L):
+    for i in range(3):
+        for j in range(3):
+            for k in range(j + 1, 3):
+                if L[i][j] > L[i][k]:
+                    L[i][j], L[i][k] = L[i][k], L[i][j]
+        print(L[i][0])
+
+L = []
+for i in range(3):
+    L.append([int(i) for i in input().split()])
+minEle(L)
