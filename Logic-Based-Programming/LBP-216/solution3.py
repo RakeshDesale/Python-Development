74fdcddd453be40670d70b25e2c29907b5785568
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-07 18:24:24 +0530

    Create solution3.py

---
+++ Logic-Based-Programming/LBP-216/solution3.py
@@ -0,0 +1,16 @@
+# Using Function
+
+# Solution:
+
+def minEle(L):
+    for i in range(3):
+        min = L[i][0]
+        for j in range(3):
+            if min > L[i][j]:
+                min = L[i][j]
+        print(min)
+
+L = []
+for i in range(3):
+    L.append([int(i) for i in input().split()])
+minEle(L)
