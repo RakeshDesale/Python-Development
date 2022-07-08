084e9bf4bd48764e10330f1679354f0e794b77d1
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-08 16:49:55 +0530

    Create solution1.py

---
+++ Logic-Based-Programming/LBP-219/solution1.py
@@ -0,0 +1,15 @@
+# Solution:
+
+L = []
+oddcount = 0
+evencount = 0
+for i in range(3):
+    L.append([int(i) for i in input().split()])
+for i in range(3):
+    for j in range(3):
+        if L[i][j] % 2 == 0 and L[i][j] != 0:
+            evencount += 1
+        elif L[i][j] % 2 != 0 and L[i][j] != 0:
+            oddcount += 1
+print(oddcount)
+print(evencount)
