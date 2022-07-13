49f33bd6f1ad520138fb8929236c326c128523d1
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-13 15:59:47 +0530

    Create solution2.py

---
+++ Logic-Based-Programming/LBP-223/solution2.py
@@ -0,0 +1,24 @@
+# Using Function
+
+# Solution:
+
+def subMatrix(L1, L2, L3):
+    for i in range(3):
+        res = []
+        for j in range(3):
+            res.append(L1[i][j] - L2[i][j])
+        L3.append(res)
+    return L3
+
+L1 = []
+L2 = []
+L3 = []
+for i in range(3):
+    L1.append([int(i) for i in input().split()])
+for i in range(3):
+    L2.append([int(i) for i in input().split()])
+L3 = subMatrix(L1, L2, L3)
+for i in range(3):
+    for j in range(3):
+        print(L3[i][j], end=' ')
+    print()
