dd0b06929ff43bebc885954c19b5dc22ad66a778
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-14 15:49:52 +0530

    Create solution2.py

---
+++ Logic-Based-Programming/LBP-226/solution2.py
@@ -0,0 +1,22 @@
+# Using Inbuilt sorted Method
+
+# Solution:
+
+L1 = [int(i) for i in input().split()]
+L2 = [int(i) for i in input().split()]
+L3 = [int(i) for i in input().split()]
+L = [L1, L2, L3]
+LL = []
+for i in range(3):
+    for j in range(3):
+        LL.append(L[i][j])
+LL = sorted(LL, reverse = True)
+k = 0
+for i in range(3):
+    for j in range(3):
+        L[i][j] = LL[k]
+        k += 1
+for i in range(3):
+    for j in range(3):
+        print(L[i][j], end=' ')
+    print()
