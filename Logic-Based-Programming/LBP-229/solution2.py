31b21c980df8b1badfe1acbe94ded5a9a84c43b7
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-15 16:23:11 +0530

    Create solution2.py

---
+++ Logic-Based-Programming/LBP-229/solution2.py
@@ -0,0 +1,21 @@
+# Using Core Logic
+
+# Solution:
+
+L1 = [int(i) for i in input().split()]
+L2 = [int(i) for i in input().split()]
+L3 = [int(i) for i in input().split()]
+L = [L1, L2, L3]
+LL = [[0,0,0],[0,0,0],[0,0,0]]
+for i in range(3):
+    for j in range(3):
+        LL[i][j] = L[j][i]
+for i in range(3):
+    for j in range(3):
+        for k in range(j + 1,3):
+            if LL[i][j] > LL[i][k]:
+                LL[i][j], LL[i][k] = LL[i][k], LL[i][j]
+for i in range(3):
+    for j in range(3):
+        print(LL[j][i], end=' ')
+    print()
