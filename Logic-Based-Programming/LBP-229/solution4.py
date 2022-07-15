3c831bca4e0365777887e6ebe37738e5c8f87dac
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-15 16:24:35 +0530

    Create solution4.py

---
+++ Logic-Based-Programming/LBP-229/solution4.py
@@ -0,0 +1,24 @@
+# Using Function and Using Core Logic
+
+# Solution:
+
+def sortColEle(L):
+    LL = [[0,0,0],[0,0,0],[0,0,0]]
+    for i in range(3):
+        for j in range(3):
+            LL[i][j] = L[j][i]
+    for i in range(3):
+        for j in range(3):
+            for k in range(j + 1,3):
+                if LL[i][j] > LL[i][k]:
+                    LL[i][j], LL[i][k] = LL[i][k], LL[i][j]
+    return LL
+
+L1 = [int(i) for i in input().split()]
+L2 = [int(i) for i in input().split()]
+L3 = [int(i) for i in input().split()]
+L = sortColEle([L1, L2, L3])
+for i in range(3):
+    for j in range(3):
+        print(L[j][i], end=' ')
+    print()
