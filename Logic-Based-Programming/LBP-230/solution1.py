53d49203072bc5d519176304807a8d8c1d43885a
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-18 20:06:24 +0530

    Create solution1.py

---
+++ Logic-Based-Programming/LBP-230/solution1.py
@@ -0,0 +1,16 @@
+# Solution:
+
+L1 = [int(i) for i in input().split()]
+L2 = [int(i) for i in input().split()]
+L3 = [int(i) for i in input().split()]
+L = [L1, L2, L3]
+LL = [[0,0,0],[0,0,0],[0,0,0]]
+for i in range(3):
+    for j in range(3):
+        LL[i][j] = L[j][i]
+for i in range(3):
+    LL[i].sort(reverse = True)
+for i in range(3):
+    for j in range(3):
+        print(LL[j][i], end=' ')
+    print()
