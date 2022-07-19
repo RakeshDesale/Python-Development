42cf4387527bf0ff9fd2952a522e88c5c3281b8a
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-19 15:10:06 +0530

    Create solution1.py

---
+++ Logic-Based-Programming/LBP-232/solution1.py
@@ -0,0 +1,13 @@
+# Solution:
+
+L1 = [int(i) for i in input().split()]
+L2 = [int(i) for i in input().split()]
+L3 = [int(i) for i in input().split()]
+m = int(input())
+n = int(input())
+L = [L1, L2, L3]
+L[m - 1], L[n - 1] = L[n - 1], L[m - 1]
+for i in range(3):
+    for j in range(3):
+        print(L[i][j], end=' ')
+    print()
