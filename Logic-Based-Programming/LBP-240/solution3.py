30b0bbf1b19c9992e43979fffb6829c147c355ac
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-28 21:03:46 +0530

    Create solution3.py

---
+++ Logic-Based-Programming/LBP-240/solution3.py
@@ -0,0 +1,14 @@
+# Using Function
+
+# Solution:
+
+def printSqr(L):
+    for i in range(3):
+        for j in range(3):
+            print(L[i][j] * L[i][j], end=' ')
+        print()
+
+L1 = [int(i) for i in input().split()]
+L2 = [int(i) for i in input().split()]
+L3 = [int(i) for i in input().split()]
+printSqr([L1, L2, L3])
