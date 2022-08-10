b89af84638209af55bafa0663eaf5ae487cd995e
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-08-10 14:48:55 +0530

    Create solution1.py

---
+++ Logic-Based-Programming/LBP-241/solution1.py
@@ -0,0 +1,9 @@
+# Solution:
+
+L = [[int(i) for i in input().split()], [int(i) for i in input().split()], [int(i) for i in input().split()]]
+sum = 0
+for i in range(3):
+    if i % 2 == 0:
+        for j in range(3):
+            sum = sum + L[i][j]
+print(sum)
