ab427e53ea25a82e4178b86a92934a1642825db5
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-08-10 14:50:55 +0530

    Create solution4.py

---
+++ Logic-Based-Programming/LBP-241/solution4.py
@@ -0,0 +1,13 @@
+# Another Approach Using Function
+
+# Solution:
+
+def sumOfEvenIndexedRows(L):
+    sum = 0
+    for i in range(3):
+        for j in range(3):
+            if i % 2 == 0:
+                sum = sum + L[i][j]
+    return sum
+
+print(sumOfEvenIndexedRows([[int(i) for i in input().split()], [int(i) for i in input().split()], [int(i) for i in input().split()]]))
