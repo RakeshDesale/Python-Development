d6dcc3def7aadd0ad1d57abf40b3defb5dcd64c9
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-08-12 15:34:49 +0530

    Create solution2.py

---
+++ Logic-Based-Programming/LBP-244/solution2.py
@@ -0,0 +1,13 @@
+# Using Function
+
+# Solution:
+
+def sumOfEvenIndexedCols(L):
+    sum = 0
+    for i in range(3):
+        for j in range(3):
+            if j % 2 != 0:
+                sum = sum + L[i][j]
+    return sum
+
+print(sumOfEvenIndexedCols([[int(i) for i in input().split()], [int(i) for i in input().split()], [int(i) for i in input().split()]]))
